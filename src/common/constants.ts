909f343fa63581427cb51e1c8947278130e21414
Zheoden <schifferjulian@gmail.com>
2022-09-21 20:31:08 -0300

    Fix S3 service

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,3 +4,7 @@ export const DB_HOSTNAME = process.env.DB_HOSTNAME ?? '';
 export const DB_USERNAME = process.env.DB_USERNAME ?? '';
 export const DB_PASSWORD = process.env.DB_PASSWORD ?? '';
 export const CDN_URL = process.env.CDN_URL ?? '';
+
+export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY ?? '';
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY ?? '';
+export const AWS_ASSETS_BUCKET = process.env.AWS_ASSETS_BUCKET ?? '';
--- src/utils/S3Service.ts
+++ src/utils/S3Service.ts
@@ -1,51 +1,43 @@
 import AWS from 'aws-sdk';
 import 'dotenv/config';
+import { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_ASSETS_BUCKET } from '../common/constants';
 
 export type AWSOperation = 'getObject' | 'putObject';
 const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
 });
 
 export class S3Service {
-  private AWS_ASSETS_BUCKET = process.env.AWS_ASSETS_BUCKET ?? '';
   uploadFile = async (file: Express.Multer.File) => {
     const { originalname } = file;
-    return await this.s3_upload(file.buffer, this.AWS_ASSETS_BUCKET, originalname, file.mimetype);
+    return await this.s3_upload(file.buffer, AWS_ASSETS_BUCKET, originalname, file.mimetype);
   };
 
-  uploadBuffer = async (file: Buffer, name: string, mimeType: string) => {
-    return await this.s3_upload(file, this.AWS_ASSETS_BUCKET, name, mimeType);
+  uploadBuffer = async (file: Buffer, name: string, mimeType: string): Promise<void> => {
+    await this.s3_upload(file, AWS_ASSETS_BUCKET, name, mimeType);
   };
 
   public async getSignedUrl(id: number, operation: AWSOperation) {
     const params = {
-      Bucket: this.AWS_ASSETS_BUCKET,
+      Bucket: AWS_ASSETS_BUCKET,
       Key: String(id),
       Expires: 300,
       ACL: 'public-read',
     };
     return await s3.getSignedUrlPromise(operation, params);
-  };
+  }
 
   public async deleteFile(id: number) {
     const params = {
-      Bucket: this.AWS_ASSETS_BUCKET,
+      Bucket: AWS_ASSETS_BUCKET,
       Key: String(id),
     };
     return await s3.deleteObject(params).promise();
-  };
+  }
 
-  /**
-   * [This is a private function and shouldn't be used]
-   * @param  {[Buffer]} file [description]
-   * @param  {[String]} bucket [description]
-   * @param  {[String]} name [description]
-   * @param  {[String]} mimetype [description]
-   * @return {[]}      [description]
-   */
-  private async s3_upload(file: Buffer, bucket: string, name: string, mimetype: string) {
+  private async s3_upload(file: Buffer, bucket: string, name: string, mimetype: string): Promise<void> {
     const params = {
       Bucket: bucket,
       Key: String(name),
@@ -57,6 +49,6 @@ export class S3Service {
         LocationConstraint: 'ap-south-1',
       },
     };
-    return await s3.upload(params).promise();
-  };
+    await s3.upload(params).promise();
+  }
 }
